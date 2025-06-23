8465a3e38da95638adc6e00bd7abd7213a84809b
karelcosta <costakarel@gmail.com>
2025-06-23 15:25:27 -0300

    Ajuste final na tela preta Feat: ajustes nos arquivos global e layot que estavam faltando

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,13 @@ import { useEffect, useState } from 'react';
 import { getScaleFactor } from '../../public/utils/utils';
 import { Code, SquareArrowOutUpRight } from "lucide-react";
 import Image from "next/image";
+
 import ProfileImage from "../../public/assets/Astronaut.jpg"
+// const ProfileImage = "/assets/Astronaut.jpg";
+
 import ProImage from "../../public/assets/ProfileTeste01.jpg"
+// const ProImage = "/assets/ProImageSmiling.jpg";
+
 import { useRouter } from "next/navigation";
 
 export default function Home() {
@@ -20,7 +25,7 @@ export default function Home() {
     return () => window.removeEventListener('resize', updateScale);
   }, []);
   const handleButtonClick = () => {
-    window.open('https://www.linkedin.com/in/pedro-henrique-queiroz-50301b221/', '_blank', 'noopener,noreferrer');
+    window.open('https://www.linkedin.com/in/karel-costa-350345337/', '_blank', 'noopener,noreferrer');
   };
 
   return (
@@ -39,6 +44,7 @@ export default function Home() {
             <span className="ml-5 h-auto text-3xl text-[#396291] flex items-center gap-4 w-full">
               <div className="xl:h-15 xl:w-15 h-[180px] w-[180px] border-white border-8 xl:border-2 rounded-[50%] overflow-hidden">
                 <Image alt="" src={ProfileImage} width={0} height={0} className="relative bottom-2.5 !w-full" />
+                {/* <Image src={ProfileImage} alt="..." width={0} height={0} className="relative bottom-2.5 !w-full" /> */}
               </div>
               <span className="w-[calc(100%-64px)] border-b-8 text-7xl xl:text-3xl xl:border-b-2 font-[family-name:var(--font-geist-mono)]">
                 PEDRO HQC_
@@ -48,11 +54,12 @@ export default function Home() {
           <div className="flex xl:!flex-row flex-col xl:gap-[250px] h-full overflow-hidden items-center">
             <div className='h-full xl:max-w-[50%] xl:overflow-auto xl:ml-[100px] overflow-hidden'>
               <Image alt="" src={ProImage} width={0} height={0} className="xl:h-[100%] xl:w-[calc(100%*1.5)] relative" />
+              {/* <Image src={ProImage} alt="..." width={0} height={0} className="relative bottom-2.5 !w-full" /> */}
             </div>
             <div className="flex flex-col xl:!h-[400px] xl:p-0 p-[100px] h-full xl:!w-[600px] w-full xl:!text-lg text-6xl font-[family-name:var(--font-geist-mono)] text-justify ">
-              <span className="xl:!text-4xl text-6xl">Hi! I&apos;m Pedro Coelho</span>
+              <span className="xl:!text-4xl text-6xl">Hi! I&apos;m Karel Costa</span>
               <span className="xl:!text-6xl text-[200px]">
-                <span className="xl:!text-7xl text-[256px] font-[family-name:var(--font-jersey-10)]">Game</span> developer
+                <span className="xl:!text-7xl text-[256px] font-[family-name:var(--font-jersey-10)]">Web</span> developer
               </span>
               Passionate and skilled C# developer with extensive experience in advanced game
               development, specializing in both 2D and 3D projects. Proficient in implementing
