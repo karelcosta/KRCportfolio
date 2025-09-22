275be8147be7b70108c6d53554e943ff0c40bda2
karelcosta <costakarel@gmail.com>
2025-09-21 23:52:33 -0300

    aaaa

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : "",
+
 };
 
 export default nextConfig;
