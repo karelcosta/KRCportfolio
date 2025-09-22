71d96b7aa8005b802692cff688b31843c4a29ec3
karelcosta <costakarel@gmail.com>
2025-09-22 00:07:55 -0300

    mias imports ainda

--- src/app/projects/Projects.tsx
+++ src/app/projects/Projects.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState, useRef } from "react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 // import Image from "next/image";
 import { getScaleFactor } from "../../../public/utils/utils";
 // import { Code, SquareArrowOutUpRight } from "lucide-react";
