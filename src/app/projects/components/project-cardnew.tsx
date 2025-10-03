b2078f623a75d99ad7705110abe8c12afa7ba07b
karelcosta <costakarel@gmail.com>
2025-10-03 14:44:16 -0300

    adição do dialog de detalhes dos porjetos ps: dialog ainda bugado no mobile

--- src/app/projects/components/project-cardnew.tsx
+++ src/app/projects/components/project-cardnew.tsx
@@ -1,7 +1,9 @@
 import Image from "next/image";
-import { useState } from "react";
 import Dialog from "./dialog";
 import { DialogData } from "../utilsClasses";
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
+import { tv } from "tailwind-variants";
+import { useRef, useState } from 'react';
 
 interface ProjectCardProps {
   title: string;
@@ -11,18 +13,61 @@ interface ProjectCardProps {
   dialogData: DialogData[];
 }
 
+export const teste = tv({
+  slots: {
+    card: "group flex flex-col-reverse mx-3 xl:w-[500px] w-[300px] xl:h-[500px] h-[300px] xl:min-w-[500px] xl:min-h-[500px] min-w-[300px] min-h-[300px] transition-all duration-180 bg-cover xl:hover:cursor-pointer xl:hover:h-[500px]  rounded-2xl bg-no-repeat bg-center",
+    cardBlur: "relative before:content-[''] before:rounded-2xl before:absolute before:inset-0 before:bg-black/70 before:backdrop-blur-sm before:opacity-0 before:transition-opacity before:duration-180 xl:hover:before:opacity-100",
+    // titleClass: "absolute w-full text-center bottom-[8px] left-1/2 -translate-x-1/2  text-white  font-[family-name:var(--font-jersey-10)] xl:text-6xl text-9xl  opacity-100 transition-all duration-180 xl:group-hover:translate-y-[-400px]",
+    titleClass: " w-full text-center     text-white  font-[family-name:var(--font-jersey-10)] xl:text-6xl text-9xl  opacity-100 transition-all duration-180 xl:group-hover:translate-y-[-400px]",
+    dialogTitleClass: "mx-auto font-[family-name:var(--font-jersey-10)] xl:text-7xl text-9xl mt-[75px] text-white",
+    descriptionClass: "text-white  indent-[30px] font-[family-name:var(--font-geist-mono)] opacity-0 transition-all duration-180  xl:group-hover:opacity-100 mx-[auto] mt-[22px] text-[20px]  w-[calc(100%*1/2*1.7777)] text-justify scroll-auto",
+    descriptionArea: "relative flex flex-col h-full",
+    // descriptionImageContainer: "opacity-0 scale-0 xl:group-hover:opacity-100 xl:group-hover:scale-100 relative w-[calc(100%*1/2*1.7777)] min-h-[calc(100%*1/1.5*0.5625)] m-auto mb-0 mt-[12px] rounded-md transition-all ease-in-out overflow-hidden",
+    descriptionImageContainer: "opacity-0 scale-0 xl:group-hover:opacity-100 xl:group-hover:scale-100 relative w-[calc(100%*1/2*1.7777)] min-h-[200px] mx-auto mt-[12px] mb-[20px] rounded-md transition-all ease-in-out overflow-hidden",
+
+    descriptionImage: "w-[calc(100%-3px)] h-[calc(100%-3px)] object-cover rounded-md justify-center m-auto my-[1.5px]",
+    borderGradient: "before:content-[''] before:absolute before:bg-amber-500 before:h-full before:w-full before:p-10 before:z-[-1] before:bg-[conic-gradient(magenta,cyan,magenta)] before:animate-custom-spin before:linear"
+  }
+});
+
+const {
+  // card,
+  // cardBlur,
+  // titleClass,
+  dialogTitleClass,
+  // descriptionArea,
+  // descriptionClass,
+  // descriptionImage,
+  // borderGradient,
+  // descriptionImageContainer
+} = teste();
+
 export default function ProjectCardnew({
   title,
   description,
   imageUrl,
   // cardImageUrl,
-  // dialogData,
+  dialogData,
 }: ProjectCardProps) {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const scroll = (direction: string) => {
+    if (containerRef.current) {
+      //@ts-expect-error null inexistent
+      containerRef.current.scrollBy({
+        top: direction === 'down' ? cardHeight + gap : -(cardHeight + gap),
+        behavior: 'smooth'
+      });
+    }
+  };
+  const containerRef = useRef(null);
+  const cardHeight = 881;
+  const gap = 0;
+
+
 
   return (
     <div className="flex flex-col xl:flex-row bg-zinc-900 rounded-2xl shadow-lg overflow-hidden mx-4 my-6 hover:shadow-xl transition-all duration-300">
-      
+
       {/* Imagem do projeto */}
       <div className="w-full xl:w-1/2 relative h-[250px] xl:h-[320px]">
         <Image
@@ -53,10 +98,10 @@ export default function ProjectCardnew({
           onClick={() => setIsDialogOpen(true)}
           className="self-start px-5 py-2 rounded-lg bg-[#396291] transition-all duration-200"
         >
-            {/* bg-orange-500 hover:bg-orange-600 */}
+          {/* bg-orange-500 hover:bg-orange-600 */}
           Ver projeto
         </button>
-        
+
       </div>
 
       {/* Dialog para detalhes */}
@@ -65,10 +110,52 @@ export default function ProjectCardnew({
         onClose={() => setIsDialogOpen(false)}
         bgImage={imageUrl}
       >
-        {/* Aqui pode reaproveitar o que você já tinha no Dialog */}
-        <div className="p-6 text-white">
-          <h2 className="text-3xl font-bold mb-4">{title}</h2>
-          <p className="text-lg">{description}</p>
+        <div className="flex flex-col w-full h-full overflow-y-scroll overflow-x-hidden" ref={containerRef}>
+          {dialogData.length > 1 && (
+            <div className='z-[101]'>
+              <button onClick={() => scroll('up')} className="xl:flex hidden absolute right-5 top-15 -translate-y-1/2 bg-white/10 text-white rounded-full w-15 h-15 items-center justify-center hover:bg-white/40 transition-all duration-300">
+                <FiChevronUp className="h-6 w-6" />
+              </button>
+              <button onClick={() => scroll('down')} className="xl:flex hidden absolute right-5 -bottom-1 -translate-y-1/2 bg-white/10 text-white rounded-full w-15 h-15 items-center justify-center hover:bg-white/40 transition-all duration-300">
+                <FiChevronDown className="h-6 w-6" />
+              </button>
+              <button onClick={() => scroll('down')} className="animate-bounce xl:hidden flex absolute right-[45%] -bottom-10 -translate-y-1/2 bg-white/10 text-white rounded-full w-[200px] h-[200px] items-center justify-center hover:bg-white/40 transition-all duration-300">
+                <FiChevronDown className="h-20 w-20" />
+              </button>
+            </div>
+          )}
+
+          {dialogData.map((item, index) => (
+            <div key={index} className='flex xl:flex-row flex-col xl:items-start items-center w-full !min-h-full h-full z-10'>
+              <span className='xl:inline relative xl:w-[33%] w-[90%] xl:h-full h-[calc(100%-1300px)] flex xl:ml-[4%]'>
+                <span className='w-[100%] absolute top-0 xl:bottom-[0] bottom-[-1300px] bg-black/40 flex flex-col gap-4	z-[-1]'>
+                  <span className={dialogTitleClass()}>{item.titleText}</span>
+                  <span className=' font-[family-name:var(--font-geist-mono)] xl:text-[20px] text-[66px] text-white px-[22px] text-justify'>
+                    <div className='indent-[30px] '>{item.firstText}</div>
+                    <br />
+                    <div className='indent-[30px]'>{item.secondText}</div>
+                    <br />
+                    <div className='indent-[30px]'>{item.thirdText}</div>
+                  </span>
+                </span>
+              </span>
+              <span className='flex flex-col gap-4 xl:w-[67%] w-full xl:h-full h-[1120px] justify-center items-center'>
+                <span className='grid xl:grid-cols-[615px_288px] grid-cols-[984px_461px] rounded-md gap-4 xl:h-[345px] h-[552] overflow-hidden'>
+                  <Image alt="" src={item.mainImage} width={1920} height={1080} className="relative rounded-md shadow-lg  xl:!h-[345px] !h-[552] object-cover" />
+                  <Image alt="" src={item.upperMinorImage} width={1920} height={1080} className="relative rounded-md shadow-lg  !w-full" />
+                </span>
+                <span className='grid xl:grid-cols-[288px_615px] grid-cols-[461px_984px] rounded-md gap-4 xl:h-[345px] h-[552] overflow-hidden'>
+                  <Image alt="" src={item.bottomMinorImage} width={1920} height={1080} className="relative rounded-md shadow-lg !w-full " />
+                  <video src={item.videoSrc}
+                    autoPlay
+                    loop
+                    width="0"
+                    height="0"
+                    className="rounded-md shadow-lg w-full"></video>
+                </span>
+              </span>
+            </div>
+          ))}
         </div>
       </Dialog>
     </div>
