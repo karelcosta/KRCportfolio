83412fa3cb8f45253750dabd8d385f53c483d460
karelcosta <costakarel@gmail.com>
2025-10-22 19:41:16 -0300

    edit nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,17 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production' //new
+const repo = 'KRCportfolio' // new
 
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: isProd ? `/${repo}` : '', // new 
+  assetPrefix: isProd ? `/${repo}/` : '', //new 
+
   images: {
     unoptimized: true,
   },
-  basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : "",
+  // basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : "",
 
 };
 
