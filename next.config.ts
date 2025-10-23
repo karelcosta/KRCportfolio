0c574bbff647875d54ce576cc8796419d5b7cb50
karelcosta <costakarel@gmail.com>
2025-10-23 15:28:30 -0300

    reset do nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : "",
 
 };
 
