70b97e5f59eb531c7a9c724d70320518ca653569
karelcosta <costakarel@gmail.com>
2025-10-24 16:42:11 -0300

    Alteração do favicon

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 // import { useRouter } from "next/navigation";
-// import Image from "next/image";
-// import IconImage from "../favicon.ico";
+import Image from "next/image";
+import IconImage from "../favicon.ico";
 
 export default function Sidebar() {
   // const router = useRouter();
@@ -16,10 +16,10 @@ export default function Sidebar() {
   return (
     <div className="  w-screen bg-[#131313] h-[80px] fixed left-0 top-0 flex flex-row justify-between   p-6 shadow-lg text-white z-50">
       <div className="flex flex-row" >
-        {/* <div className="h-full xl:max-w-[100%] overflow-hidden">
+        <div className="h-full xl:max-w-[100%] overflow-hidden">
           <Image alt="dev" src={IconImage} width={0} height={0} className="xl:h-[100%] xl:w-[calc(100%*1.5)] relative" />
-        </div> */}
-        <h1 className="text-3xl font-bold mb-10"> Karel</h1>
+        </div>
+        <h1 className="text-3xl font-bold mb-10"> arel</h1>
       </div>
       <div className="flex gap-6 ">
         <button onClick={() => scrollTo("home")}>Home</button>
