51af9367998b179e577819ac07d13129e75faf15
karelcosta <costakarel@gmail.com>
2025-10-24 14:45:14 -0300

    Mais ajustes

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
   /* config options here */
 
   output: 'export',
+  basePath: isProd ? '/KRCportfolio' : '',
+  assetPrefix: isProd ? '/KRCportfolio/' : '',
+  images: { unoptimized: true },
 
 };
 
