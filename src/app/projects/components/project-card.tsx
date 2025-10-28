441b77b055ebb2daaf03fd5cb52b2f48ed9ff94b
karelcosta <costakarel@gmail.com>
2025-10-28 16:03:07 -0300

    pequenos ajustes

--- src/app/projects/components/project-card.tsx
+++ src/app/projects/components/project-card.tsx
@@ -39,42 +39,29 @@ export default function ProjectCard({
 
 
   return (
-    <div className="flex flex-col xl:flex-row bg-zinc-900 rounded-2xl shadow-lg overflow-hidden mx-4 my-6 hover:shadow-xl transition-all duration-300">
+    <div >
 
-      {/* Imagem do projeto */}
-      <div className="w-full xl:w-1/2 relative h-[250px] xl:h-[320px]">
-        <Image
-          src={imageUrl}
-          alt={title}
-          fill
-          className="object-cover"
-        />
-      </div>
-
-      {/* Conteúdo textual */}
-      <div className="w-full xl:w-1/2 flex flex-col justify-center p-6 text-white">
-        <h2 className="text-2xl font-bold mb-3">{title}</h2>
-        <p className="text-base text-gray-300 mb-4">{description}</p>
-
-        {/* Tecnologias */}
-        {/* <div className="flex flex-wrap gap-3 mb-4 text-sm">
-          <span className="px-3 py-1 rounded-md bg-orange-600">HTML</span>
-          <span className="px-3 py-1 rounded-md bg-blue-600">CSS</span>
-          <span className="px-3 py-1 rounded-md bg-yellow-500 text-black">
-            JavaScript
-          </span>
-          <span className="px-3 py-1 rounded-md bg-purple-600">Bootstrap</span>
-        </div> */}
-
-        {/* Botão */}
-        <button
-          onClick={() => setIsDialogOpen(true)}
-          className="self-start px-5 py-2 rounded-lg bg-[#e51c23] border-3 border-[#b0120a] hover:border-[#4a148c] hover:bg-[#b0120a] transition-all duration-200"
-        >
-          {/* bg-orange-500 hover:bg-orange-600 */}
-          Ver projeto
-        </button>
-
+      <div className="flex flex-col xl:flex-row bg-zinc-900 rounded-2xl shadow-lg overflow-hidden mx-4 my-6 hover:shadow-xl transition-all duration-300">
+        <div className="w-full xl:w-1/2 relative h-[250px] xl:h-[320px]">
+          <Image
+            src={imageUrl}
+            alt={title}
+            fill
+            className="object-cover"
+          />
+        </div>
+        <div className="w-full xl:w-1/2 flex flex-col justify-center p-6 text-white">
+          <h2 className="text-2xl font-bold mb-3">{title}</h2>
+          <p className="text-base text-gray-300 mb-4">{description}</p>
+          <button
+            onClick={() => setIsDialogOpen(true)}
+            className="self-start px-5 py-2 rounded-lg bg-[#e51c23] border-3 border-[#b0120a] hover:border-[#4a148c] hover:bg-[#b0120a] transition-all duration-200"
+          >
+            {/* bg-orange-500 hover:bg-orange-600 */}
+            Ver projeto
+          </button>
+        </div>
+        
       </div>
 
       {/* Dialog para detalhes */}
