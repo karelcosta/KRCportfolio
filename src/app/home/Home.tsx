28ffe7ba051b0ce44a6864aa8ea06177be014565
karelcosta <costakarel@gmail.com>
2025-11-26 17:45:31 -0300

    AAAAAAA

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -14,6 +14,13 @@ export default function Home() {
   // const router = useRouter();
   const [scale, setScale] = useState(1);
 
+  const scrollTo = (id: string) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   useEffect(() => {
     const updateScale = () => setScale(getScaleFactor());
     updateScale();
@@ -22,17 +29,17 @@ export default function Home() {
     return () => window.removeEventListener("resize", updateScale);
   }, []);
 
-  const handleButtonHireMeClick = () => {
-    window.open("https://www.linkedin.com/in/karel-costa-350345337/", "_blank", "noopener,noreferrer");
-  };
-  const handleButtonGitHubClick = () => {
-    window.open("https://github.com/karelcosta", "_blank", "noopener,noreferrer");
-  };
-// text-[#4a148c] roxo
-// text-[#e51c23] vermelho
-// bg-[#CCCCCC]
-// 0f0f0f - cinza mais escuro
-//1E1E1E - cinza claro
+  // const handleButtonHireMeClick = () => {
+  //   window.open("https://www.linkedin.com/in/karel-costa-350345337/", "_blank", "noopener,noreferrer");
+  // };
+  // const handleButtonGitHubClick = () => {
+  //   window.open("https://github.com/karelcosta", "_blank", "noopener,noreferrer");
+  // };
+  // text-[#4a148c] roxo
+  // text-[#e51c23] vermelho
+  // bg-[#CCCCCC]
+  // 0f0f0f - cinza mais escuro
+  //1E1E1E - cinza claro
   return (
     <div className=" w-full min-h-screen bg-[#0f0f0f] text-black relative overflow-hidden">
       <div
@@ -43,7 +50,7 @@ export default function Home() {
           height: `${100 / scale}%`,
         }}
       >
-        <div className="flex flex-col h-full w-full mu-[30px]">          
+        <div className="flex flex-col h-full w-full mu-[30px]">
           {/* amostruario do portifolio */}
           <div className="flex flex-col xl:flex-row xl:gap-[60px] h-full overflow-hidden items-center p-10 justify-center">
             <div className="h-full xl:max-w-[50%] overflow-hidden">
@@ -52,17 +59,29 @@ export default function Home() {
             <div className="flex flex-col xl:h-[400px] h-full xl:w-[600px] w-full xl:text-lg text-4xl font-[family-name:var(--font-geist-mono)] text-justify mt-10 xl:mt-0 text-[#FFFFFF]">
               <span className="text-5xl mb-4 text-center ">Hi! I&apos;m Karel Costa</span>
               <span className="text-7xl font-[family-name:var(--font-jersey-10)] mb-6 text-center text-[#e51c23]">Game & Web developer</span>
-              Desenvolvedor C# dedicado e em constante evolução, com foco principal no desenvolvimento de jogos 2D alem
-              de uma leve experiência em projetos 3D. Além da área de jogos, atuo também no desenvolvimento web, buscando
-              sempre aplicar boas práticas e aprimorar minhas habilidades. Tenho como objetivo criar soluções criativas e
-              funcionais, conciliando aprendizado contínuo com a entrega de resultados de qualidade.
+              Desenvolvedor Full-Stack e de jogos com 2 anos de experiência, formado em Ciência da Computação.
+              Atuando com Typescript/Node.js e Unity.
+              Experiência prática em implementação de funcionalidades, refatoração, manutenção, criação de mecânicas e protótipos,
+              com foco em soluções eficientes, código limpo, aplicação de práticas de DevOps e aprendizado contínuo.
               <div className="flex xl:gap-10 gap-4 justify-between mt-10 h-auto">
-                <button onClick={handleButtonGitHubClick} className="transform active:scale-90 flex items-center justify-center gap-4 w-full border-4 px-4 py-3 bg-[#e51c23] border-[#b0120a]  text-white rounded-xl hover:border-[#4a148c] hover:bg-[#b0120a] transition text-xl">
+                {/* <button onClick={handleButtonGitHubClick} className="transform active:scale-90 flex items-center justify-center gap-4 w-full border-4 px-4 py-3 bg-[#e51c23] border-[#b0120a]  text-white rounded-xl hover:border-[#4a148c] hover:bg-[#b0120a] transition text-xl">
                   GitHub <Code className="w-6 h-6" />
+                </button> */}
+                <button onClick={() => scrollTo("projects")} className="transform active:scale-90 flex items-center justify-center gap-4 w-full border-4 px-4 py-3 bg-[#e51c23] border-[#b0120a]  text-white rounded-xl hover:border-[#4a148c] hover:bg-[#b0120a] transition text-xl">
+                  Projetos <Code className="w-6 h-6" />
                 </button>
-                <button onClick={handleButtonHireMeClick} className="transform active:scale-90 flex items-center justify-center gap-4 w-full px-4 py-3 border-4 border-[#e51c23] text-[#FFFFFF] rounded-xl hover:border-[#4a148c] hover:bg-[#e51c23] hover:text-white transition text-xl font-bold">
+                {/* <button onClick={handleButtonHireMeClick} className="transform active:scale-90 flex items-center justify-center gap-4 w-full px-4 py-3 border-4 border-[#e51c23] text-[#FFFFFF] rounded-xl hover:border-[#4a148c] hover:bg-[#e51c23] hover:text-white transition text-xl font-bold">
                   LinkedIn  <SquareArrowOutUpRight className="w-6 h-6" />
-                </button>
+                </button> */}
+                <a
+                  href="/Currículo - Karel Ribeiro Costa.pdf"
+                  download
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="transform active:scale-90 flex items-center justify-center gap-4 w-full px-4 py-3 border-4 border-[#e51c23] text-[#FFFFFF] rounded-xl hover:border-[#4a148c] hover:bg-[#e51c23] hover:text-white transition text-xl font-bold"
+                >
+                  Baixar CV <SquareArrowOutUpRight className="w-6 h-6" />
+                </a>
               </div>
             </div>
           </div>
